2f67199f1e2c23c62ad80709c27c9776c01f3e35
yeojining5 <yeojining5@gmail.com>
2022-10-13 23:44:05 +0900

    221013 React 연동 - jsonDeptList

--- demo0921/src/main/java/com/example/demo/controller/RestDeptController.java
+++ demo0921/src/main/java/com/example/demo/controller/RestDeptController.java
@@ -12,37 +12,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.demo.logic.DeptLogic;
 import com.example.demo.vo.DeptVO;
+import com.google.gson.Gson;
 
-@Controller
+// react와 연동하는 Controller는 @RestController 어노테이션을 사용한다.
+@RestController
+
 @RequestMapping("/dept/*")
-@SessionAttributes({"s_id","s_name"})
-public class DeptController {
-   Logger logger = LogManager.getLogger(DeptController.class);
+public class RestDeptController {
+   Logger logger = LogManager.getLogger(RestDeptController.class);
+   
    @Autowired
    private DeptLogic deptLogic = null;
    
-   @ResponseBody
-   @GetMapping("deptInsert")
-   public String deptInsert(@RequestParam Map<String, Object> pMap) {
-	   logger.info(pMap);
-	   int result = 0;
-	   result = deptLogic.deptInsert(pMap);
-	   return ""+result; // 문자열 붙이면 String 타입으로 형전환
-   }
-   
-   @GetMapping("deptList")
-   public String deptList(Model model, @RequestParam Map<String,Object> pMap) {
+   @GetMapping("jsonDeptList")
+   public String DeptList(Model model, @RequestParam Map<String, Object> pMap) {
       logger.info("deptList 호출 성공");
       List<Map<String, Object>> deptList = null;
-      pMap.put("deptno", 20);
       deptList = deptLogic.deptList(pMap);
-      model.addAttribute("deptList", deptList);
-//      return "dept/deptList";
-      return "forward:deptList.jsp";
+      logger.info(deptList);
+      String temp = null;
+      Gson g = new Gson();
+      temp = g.toJson(deptList);
+      return temp;
    }
-   
 }
--- demo0921/src/main/java/com/example/demo/dao/DeptDao.java
+++ demo0921/src/main/java/com/example/demo/dao/DeptDao.java
@@ -9,26 +9,37 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.logic.DeptLogic;
 import com.example.demo.vo.DeptVO;
+
 
 @Service
 public class DeptDao {
-	Logger logger = LogManager.getLogger(DeptDao.class);
-	
-	@Autowired
-	private SqlSessionTemplate sqlSessionTemplate = null;
-	
-	public List<DeptVO> deptList(Map<String, Object> pMap) {
-		logger.info(pMap.get("deptno"));
-		List<DeptVO> deptList = null;
-		sqlSessionTemplate.selectOne("deptList", pMap);
-		logger.info("pMap : "+pMap);
-		deptList = (List<DeptVO>)pMap.get("key");
-		for(int i=0; i<deptList.size(); i++) {
-			DeptVO dvo = deptList.get(i);
-			logger.info(dvo.getDeptno());
-		}
-		return deptList;
-	}
+   Logger logger = LogManager.getLogger(DeptDao.class);
+   @Autowired
+   private SqlSessionTemplate sqlSessionTemplate = null;
+   public List<Map<String, Object>> deptList(Map<String, Object> pMap) {
+      logger.info(pMap.get("deptno"));
+      List<Map<String, Object>> deptList = null;
+      deptList = sqlSessionTemplate.selectList("deptList",pMap);
+      logger.info("pMap : "+pMap);
+      return deptList;
+   }
+   public List<DeptVO> deptList2(Map<String, Object> pMap) {
+      logger.info(pMap.get("deptno"));
+      List<DeptVO> deptList = null;
+      sqlSessionTemplate.selectOne("deptList2",pMap);
+      logger.info("pMap : "+pMap);
+      deptList = (List<DeptVO>)pMap.get("key");
+      for(int i=0;i<deptList.size();i++) {
+         DeptVO dvo = deptList.get(i);
+         logger.info(dvo.getDeptno());
+      }
+      return deptList;
+   }
+public int deptInsert(Map<String, Object> pMap) {
+	int result = 0;
+    result = sqlSessionTemplate.update("deptInsert",pMap);
+	return result;
+}
+   
 }
